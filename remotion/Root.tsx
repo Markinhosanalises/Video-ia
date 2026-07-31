8b731a77480887837147e5123aacf1405233b3e9
Markinhosanalises <marcosprimeconsultas@gmail.com>
2026-07-31 10:53:58 -0300

    Update Root.tsx

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -4,17 +4,22 @@ import { MyVideo } from "./MyVideo";
 import manifest from "../public/manifest.json";
 
 const FPS = manifest.fps ?? 30;
-const totalSeconds = manifest.cenas.reduce(
+const TRANSITION_FRAMES = 15;
+
+const totalSecondsCenas = manifest.cenas.reduce(
   (acc, c) => acc + c.durationSeconds,
   0
 );
+const totalFramesCenas = Math.round(totalSecondsCenas * FPS);
+const totalFrames =
+  totalFramesCenas - (manifest.cenas.length - 1) * TRANSITION_FRAMES;
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="DaviGolias"
       component={MyVideo}
-      durationInFrames={Math.round(totalSeconds * FPS)}
+      durationInFrames={totalFrames}
       fps={FPS}
       width={1080}
       height={1920}
